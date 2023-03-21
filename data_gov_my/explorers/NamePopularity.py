2152d2e9525784b87ae84b2185f13cdc3cc09dd0
roshenmaghhan <roshenmaghhan@gmail.com>
2023-03-21 17:03:09 +0800

    Added new params to Name popularity dashboard

--- data_gov_my/explorers/NamePopularity.py
+++ data_gov_my/explorers/NamePopularity.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import operator
 from django.apps import apps
 from django.http import JsonResponse
 from data_gov_my.explorers.General import General_Explorer
@@ -53,21 +54,33 @@ class NAME_POPULARITY(General_Explorer) :
 
         if (type != 'first' and type != 'last') or dashboard != self.explorer_name:
             return JsonResponse({"status": 400, "message": "Bad Request"}, status=400)
-
-        type = type.lower()
         
         model_name = self.param_models[ type ]
         model_choice = apps.get_model('data_gov_my', model_name)   
-        res = model_choice.objects.all().filter(name=s).values()
 
-        fin = {}    
-        if len(res) > 0 :
-            res = res[0] 
-            fin['name'] = res['name']
-            fin['total'] = res['total']
-            res.pop('name')
-            res.pop('total')
-            fin['decade'] = [d.replace("d_", "") for d in list(res.keys())] 
-            fin['count'] = list(res.values())
+        compare = 'compare_name' in params and self.str2bool(params['compare_name'][0])
+        
+        s = list(set(s.split(","))) if compare else [s]
+        res = model_choice.objects.all().filter(name__in=s).values()
+
+        fin = [] # Default is as a list
+
+        if len(res) > 0 : 
+            for i in res : 
+                temp = {}
+                temp['name'] = i['name']
+                temp['total'] = i['total']
+                i.pop('name')
+                i.pop('total')
+                
+                if compare :               
+                    temp['max'] = [key.replace('d_', '') for m in [max(i.values())] for key,val in i.items() if val == m][-1]
+                    temp['min'] = [key.replace('d_', '') for m in [min(i.values())] for key,val in i.items() if val == m][-1]
+                    fin.append(temp)
+                else : 
+                    temp['decade'] = [d.replace("d_", "") for d in list(i.keys())]
+                    temp['count'] = list(i.values())
+                    fin = temp # Convert back into Dictionary
+                    break
         
         return JsonResponse(fin, safe=False, status=200)
