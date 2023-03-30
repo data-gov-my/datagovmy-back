841a0970b8e262d55220ca64f07de752a52432d2
heheheejin <e_jaxx@hotmail.com>
2023-03-30 10:59:56 +0800

    Provide dates array and remove groupByDay param

--- data_gov_my/api_handling/handle.py
+++ data_gov_my/api_handling/handle.py
@@ -8,28 +8,40 @@ not covered by Meta Json
 '''
 
 
-def aggregate_sum(epochs: List[int], births: List[int], start=int, end=int, groupByDay=True):
+def dates_in_year(year: int):
     """
-    aggregate different dates by day/month across years between [start, end]
+    Given a year, return a list of dates (epoch milliseconds) within that year.
+    A non-leap year returns 365 dates; leap year returns 366 dates in a single list.
+    """
+    start = datetime(year, 1, 1)
+    end = datetime(year, 12, 31)
+    epoch = datetime.utcfromtimestamp(0)
+    dates = []
+    while start <= end:
+        epoch_ms = int((start - epoch).total_seconds() * 1000)
+        dates.append(epoch_ms)
+        start += timedelta(days=1)
+    return dates
+
+
+def aggregate_sum(epochs: List[int], births: List[int], start=int, end=int):
+    """
+    Aggregate different dates by day across years between [start, end]
     """
     FIST_VALID_DATE = datetime(1970, 1, 1)
     hasLeap = any(calendar.isleap(y) for y in range(start, end + 1))
     start, end = datetime(year=start, month=1, day=1), datetime(
         year=end, month=12, day=31)
-    n = 365 + hasLeap if groupByDay else 12
-    count = [0]*n
+    count = [0]*(365 + hasLeap)
 
     for i, e in enumerate(epochs):
         date = FIST_VALID_DATE + timedelta(milliseconds=e)
         if start <= date <= end:
-            if groupByDay:
-                pos = date.timetuple().tm_yday + (hasLeap and not calendar.isleap(date.year)
-                                                  and date.timetuple().tm_yday > 59)  # 59 = 1 March
-            else:
-                pos = date.month
+            pos = date.timetuple().tm_yday + (hasLeap and not calendar.isleap(date.year)
+                                              and date.timetuple().tm_yday > 59)  # 59 = 1 March
             count[pos - 1] += births[i]
 
-    return {"x": list(range(1, n+1)), "y": count}
+    return {"x": dates_in_year(1972) if hasLeap else dates_in_year(1970), "y": count}
 
 
 def dashboard_additional_handling(params, res):
@@ -46,12 +58,11 @@ def dashboard_additional_handling(params, res):
             return res
         return res
     if dashboard == "birthday_popularity":
-        if {"start", "end", "groupByDay"} <= params.keys():
+        if {"start", "end"} <= params.keys():
             res = aggregate_sum(epochs=res["timeseries"]["data"]["x"],
                                 births=res["timeseries"]["data"]["births"],
                                 start=int(params["start"][0]),
-                                end=int(params["end"][0]),
-                                groupByDay=params["groupByDay"][0] == "true")
+                                end=int(params["end"][0]))
         return res
 
     else:  # Default if no additions to make
