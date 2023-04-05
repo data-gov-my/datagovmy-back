42df4e85cb6954335e7315004a7c90c2ca5cbd6d
heheheejin <e_jaxx@hotmail.com>
2023-04-05 17:33:54 +0800

    Refactor birthday endpoint from dashboard to explorer

--- data_gov_my/api_handling/handle.py
+++ data_gov_my/api_handling/handle.py
@@ -7,71 +7,6 @@ Build methods for any post-handling / additional info,
 not covered by Meta Json
 '''
 
-
-def dates_in_year(year: int):
-    """
-    Given a year, return a list of dates (epoch milliseconds) within that year.
-    A non-leap year returns 365 dates; leap year returns 366 dates in a single list.
-    """
-    start = datetime(year, 1, 1)
-    end = datetime(year, 12, 31)
-    epoch = datetime.utcfromtimestamp(0)
-    dates = []
-    while start <= end:
-        epoch_ms = int((start - epoch).total_seconds() * 1000)
-        dates.append(epoch_ms)
-        start += timedelta(days=1)
-    return dates
-
-
-def dates_in_month():
-    """
-    Return a list of dates (epoch milliseconds) for every 
-    first day of every month within a year starting from epoch millisecond 0.
-    """
-    start = datetime(1971, 1, 1)  # 1971 is an arbitrary year
-    dates = []
-    for month in range(1, 13):
-        first_day_in_month = datetime(1971, month, 1)
-        epoch_ms = int((first_day_in_month - start).total_seconds() * 1000)
-        dates.append(epoch_ms)
-    return dates
-
-
-def aggregate_sum(epochs: List[int], births: List[int], ranks: List[int], start:int, end:int, birthday: datetime, groupByDay=True):
-    """
-    Aggregate different dates by day across years between [start, end]
-    Also returns the correct rank based on the input birthday and ranks list
-    """
-    if not start:
-        start = (datetime.utcfromtimestamp(0) + timedelta(milliseconds=epochs[0])).year
-    if not end:
-        end = (datetime.utcfromtimestamp(0) + timedelta(milliseconds=epochs[-1])).year
-
-    hasLeap = any(calendar.isleap(y) for y in range(start, end + 1))
-    start, end = datetime(year=start, month=1, day=1), datetime(
-        year=end, month=12, day=31)
-    count = [0]*(365 + hasLeap) if groupByDay else [0]*12
-
-    rank = None
-    for i, e in enumerate(epochs):
-        date = datetime.utcfromtimestamp(0) + timedelta(milliseconds=e)
-        if date == birthday:
-            rank =  ranks[i]
-            state_total = births[i]
-        if start <= date <= end:
-            pos = date.timetuple().tm_yday + (hasLeap and not calendar.isleap(date.year)
-                                              and date.timetuple().tm_yday > 59) if groupByDay else date.month  # 59 = 1 March
-            count[pos - 1] += births[i]
-
-    if groupByDay:
-        valid_dates = dates_in_year(1972) if hasLeap else dates_in_year(1970)
-    else:
-        valid_dates = dates_in_month()
-
-    return {"x": valid_dates, "y": count, "rank": rank, "state_total": state_total} if rank and state_total else {"x": valid_dates, "y": count}
-
-
 def dashboard_additional_handling(params, res):
     dashboard = params['dashboard'][0]
 
@@ -85,25 +20,6 @@ def dashboard_additional_handling(params, res):
             res['jitter_chart']['data'] = temp
             return res
         return res
-    if dashboard == "birthday_popularity":
-        start = int(params["start"][0]) if "start" in params else None
-        end = int(params["end"][0]) if "end" in params else None
-        groupByDay = params["groupByDay"][0] == "true" if "groupByDay" in params else True
-        state = params["state"][0]
-        input_birthday = datetime.strptime(params["birthday"][0], "%Y-%m-%d") if "birthday" in params.keys() else None
-
-        newRes = aggregate_sum(epochs=res["timeseries"]["data"]["x"],
-                            births=res["timeseries"]["data"]["births"],
-                            ranks=res["timeseries"]["data"]["rank"],
-                            start=start,
-                            end=end,
-                            birthday=input_birthday,
-                            groupByDay=groupByDay)
-        
-        if "birthday" in params:
-            # find out most and least popular birthday based on the state and birthday year
-            newRes["popularity"] = res["rank_table"]["data"][state][params["birthday"][0].split("-")[0]]
-        return newRes
     
     else:  # Default if no additions to make
         return res
---
+++ data_gov_my/explorers/BirthdayPopularity.py
@@ -0,0 +1,105 @@
+import calendar
+from datetime import datetime, timedelta
+from typing import List
+from data_gov_my.explorers.General import General_Explorer
+from rest_framework import exceptions
+from django.http import JsonResponse
+from django.apps import apps
+from data_gov_my.models import DashboardJson
+
+
+
+
+class BIRTHDAY_POPULARITY(General_Explorer):
+
+    # General Data    
+    explorer_name = "BIRTHDAY_POPULARITY"
+
+    # API handling
+    required_params = ["explorer", "state"]
+
+    def __init__(self):
+        General_Explorer.__init__(self)
+    
+    def dates_in_year(self, year: int):
+        """
+        Given a year, return a list of dates (epoch milliseconds) within that year.
+        A non-leap year returns 365 dates; leap year returns 366 dates in a single list.
+        """
+        start = datetime(year, 1, 1)
+        end = datetime(year, 12, 31)
+        epoch = datetime.utcfromtimestamp(0)
+        dates = []
+        while start <= end:
+            epoch_ms = int((start - epoch).total_seconds() * 1000)
+            dates.append(epoch_ms)
+            start += timedelta(days=1)
+        return dates
+
+    def dates_in_month(self):
+        """
+        Return a list of dates (epoch milliseconds) for every 
+        first day of every month within a year starting from epoch millisecond 0.
+        """
+        start = datetime(1971, 1, 1)  # 1971 is an arbitrary year
+        dates = []
+        for month in range(1, 13):
+            first_day_in_month = datetime(1971, month, 1)
+            epoch_ms = int((first_day_in_month - start).total_seconds() * 1000)
+            dates.append(epoch_ms)
+        return dates
+
+    def  handle_api(self, request_params):
+        """
+        Handles the API requests, and returns the data accordingly.
+        """
+        if not self.is_params_exist(request_params):
+            return JsonResponse({"status": 400, "message": "Bad Request"}, status=400)
+        
+        state = request_params["state"][0]
+        
+       # handle the data 
+        timeseries = DashboardJson.objects.get(dashboard_name="birthday_popularity", chart_name="timeseries").chart_data
+        rank_table = DashboardJson.objects.get(dashboard_name="birthday_popularity", chart_name="rank_table").chart_data
+        
+        epochs = timeseries["data"][state]["x"]
+        nationwide_births = timeseries["data"]["mys"]["births"]
+        births = timeseries["data"][state]["births"]
+        ranks= timeseries["data"][state]["rank"]
+
+        # handle the query parameters (provide default if not given)
+        start = int(request_params["start"][0]) if "start" in request_params else (datetime.utcfromtimestamp(0) + timedelta(milliseconds=epochs[0])).year
+        end = int(request_params["end"][0]) if "end" in request_params else (datetime.utcfromtimestamp(0) + timedelta(milliseconds=epochs[-1])).year
+        groupByDay = self.str2bool(request_params["groupByDay"][0]) if "groupByDay" in request_params else True
+        birthday = datetime.strptime(request_params["birthday"][0], "%Y-%m-%d") if "birthday" in request_params else None
+        
+        hasLeap = any(calendar.isleap(y) for y in range(start, end + 1))
+        start, end = datetime(year=start, month=1, day=1), datetime(
+        year=end, month=12, day=31)
+        count = [0]*(365 + hasLeap) if groupByDay else [0]*12
+        
+        # aggregate births by day or month across years within start and end date range 
+        res = {}
+        for i, e in enumerate(epochs):
+            date = datetime.utcfromtimestamp(0) + timedelta(milliseconds=e)
+            if date == birthday:
+                res["rank"] = ranks[i]
+                res["state_total"] = births[i]
+                res["nationwide_total"] = nationwide_births[i]
+            if start <= date <= end:
+                pos = date.timetuple().tm_yday + (hasLeap and not calendar.isleap(date.year)
+                                                and date.timetuple().tm_yday > 59) if groupByDay else date.month  # 59 = 1 March
+                count[pos - 1] += births[i]
+
+        if groupByDay:
+            valid_dates = self.dates_in_year(1972) if hasLeap else self.dates_in_year(1970)
+        else:
+            valid_dates = self.dates_in_month()
+
+        res["x"] = valid_dates
+        res["y"] = count
+        if birthday is not None:
+             res["popularity"] = rank_table["data"][state][str(birthday.year)]
+        return JsonResponse(res,status=200) 
+
+        
