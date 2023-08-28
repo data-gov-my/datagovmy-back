f28b892260c4474857718926819791bb56dcb5a2
roshenmaghhan <roshenmaghhan@gmail.com>
2023-08-28 16:16:29 +0800

    feat : Exclude any admin paths

--- data_gov_my/middleware/auth_middleware.py
+++ data_gov_my/middleware/auth_middleware.py
@@ -11,23 +11,32 @@ class AuthMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        if self.is_admin_panel(request) : 
+            return self.get_response(request)
+        
         if 'Authorization' in request.headers : 
             return self.get_response(request)
         
         return JsonResponse({"status" : 401, "message" : "Unauthorized"}, status=400)
     
     def process_view(self, request, view_func, view_args, view_kwargs) : 
-        view_name = view_func.view_class.__name__
-        req_auth_key = request.headers.get("Authorization")
-        master_token = os.getenv("WORKFLOW_TOKEN")
+        if not self.is_admin_panel(request=request) :         
+            view_name = view_func.view_class.__name__
+            req_auth_key = request.headers.get("Authorization")
+            master_token = os.getenv("WORKFLOW_TOKEN")
 
-        if view_name not in self._exclude : 
-            auth_key = cache.get("AUTH_KEY")
-            if not auth_key : 
-                auth_key = AuthTable.objects.filter(key="AUTH_TOKEN").values('value').first()['value']
-                cache.set("AUTH_KEY", auth_key)
-            if (req_auth_key != auth_key) and (req_auth_key != master_token) : 
-                return JsonResponse({"status" : 401, "message" : "Unauthorized"}, status=400)
-        else : 
-            if master_token != req_auth_key : 
-                return JsonResponse({"status" : 401, "message" : "Unauthorized"}, status=400)
+            if view_name not in self._exclude : 
+                auth_key = cache.get("AUTH_KEY")
+                if not auth_key : 
+                    auth_key = AuthTable.objects.filter(key="AUTH_TOKEN").values('value').first()['value']
+                    cache.set("AUTH_KEY", auth_key)
+                if (req_auth_key != auth_key) and (req_auth_key != master_token) : 
+                    return JsonResponse({"status" : 401, "message" : "Unauthorized"}, status=400)
+            else : 
+                if master_token != req_auth_key : 
+                    return JsonResponse({"status" : 401, "message" : "Unauthorized"}, status=400)
+            
+    def is_admin_panel(self, request) : 
+        if '/admin/' in request.path_info : 
+            return True
+        return False
