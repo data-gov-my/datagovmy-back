8b4489014e9d3690bec0419eea233735520b1aee
eujiaxin <e_jaxx@hotmail.com>
2023-12-01 18:23:05 +0800

    feat: create data request model

--- data_request/models.py
+++ data_request/models.py
@@ -1,3 +1,25 @@
 import uuid
 from django.db import models
 
+
+# Create your models here.
+class DataRequest(models.Model):
+    STATUS_CHOICES = [
+        ("submitted", "Submitted"),
+        ("under_review", "Under Review"),
+        ("rejected", "Rejected"),
+        ("in_progress", "In Progress"),
+        ("data_published", "Data Published"),
+    ]
+
+    ticket_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    institution = models.CharField(max_length=255, blank=True, null=True)
+    dataset_title = models.CharField(max_length=255)
+    dataset_description = models.TextField()
+    agency = models.CharField(max_length=255)
+    purpose_of_request = models.CharField(max_length=255)
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default="submitted"
+    )
