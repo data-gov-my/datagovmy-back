3b8bce23a47a65b662ad96f436d2452f7de41ac2
eujiaxin <e_jaxx@hotmail.com>
2023-12-11 13:59:49 +0800

    feat: add new dc models & modeltranslation

--- data_catalogue/admin.py
+++ data_catalogue/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
+from data_catalogue.models import *
 
-# Register your models here.
+
+# Custom admin classes for translated models
+
+
+# Register your models with the custom admin classes
+admin.site.register(DataCatalogue)
+admin.site.register(DataCatalogueMeta, TranslationAdmin)
+admin.site.register(SiteCategory, TranslationAdmin)
+admin.site.register(Field, TranslationAdmin)
+admin.site.register(RelatedDataset, TranslationAdmin)
---
+++ data_catalogue/translation.py
@@ -0,0 +1,27 @@
+from modeltranslation.translator import register, TranslationOptions
+from .models import (
+    DataCatalogueMeta,
+    Field,
+    RelatedDataset,
+    SiteCategory,
+)
+
+
+@register(Field)
+class FieldTranslationOptions(TranslationOptions):
+    fields = ("title", "description")
+
+
+@register(RelatedDataset)
+class RelatedDatasetTranslationOptions(TranslationOptions):
+    fields = ("title", "description")
+
+
+@register(SiteCategory)
+class SiteCategoryTranslationOptions(TranslationOptions):
+    fields = ("category", "subcategory")
+
+
+@register(DataCatalogueMeta)
+class DataCatalogueTranslationOptions(TranslationOptions):
+    fields = ("description", "methodology", "caveat", "publication")
