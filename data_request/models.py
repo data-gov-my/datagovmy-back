362f2356ccc612587395f95351a8c565dcc94923
eujiaxin <e_jaxx@hotmail.com>
2023-12-19 13:55:17 +0800

    feat: add conditional validation on ticket status change

--- data_request/models.py
+++ data_request/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from data_catalogue.models import DataCatalogueMeta
 
 
 # Create your models here.
@@ -25,3 +27,22 @@ class DataRequest(models.Model):
 
     def __str__(self) -> str:
         return f"{self.ticket_id} ({self.dataset_title})"
+
+    def clean(self) -> None:
+        if self.status == "rejected" and not self.rejection_reason:
+            raise ValidationError(
+                {
+                    "rejection_reason": 'Rejection reason is required when the status is "rejected".'
+                }
+            )
+
+        if self.status == "data_published":
+            linked_catalogues_count = DataCatalogueMeta.objects.filter(
+                data_request=self
+            ).count()
+            if linked_catalogues_count < 1:
+                raise ValidationError(
+                    'At least one data catalogue must be linked when the status is "data_published".'
+                )
+
+        return super().clean()
