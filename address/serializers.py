abcc2ae42a6719a1a5a48af10e70fbf61d320205
eujiaxin <e_jaxx@hotmail.com>
2024-01-19 00:15:36 +0800

    hotfix: serialise decimal fields into number type

--- address/serializers.py
+++ address/serializers.py
@@ -3,6 +3,23 @@ from .models import Address
 
 
 class AddressSerializer(serializers.ModelSerializer):
+    lat = serializers.DecimalField(
+        max_digits=10, decimal_places=7, coerce_to_string=False
+    )
+    lon = serializers.DecimalField(
+        max_digits=10, decimal_places=7, coerce_to_string=False
+    )
+
     class Meta:
         model = Address
-        exclude = ["id"]
+        fields = [
+            "address",
+            "district",
+            "subdistrict",
+            "locality",
+            "parlimen",
+            "dun",
+            "lat",
+            "lon",
+            "postcode",
+        ]
