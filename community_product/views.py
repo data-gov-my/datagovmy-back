7e60b68f9a399168d80d5401f24df65bdd65fab1
eujiaxin <e_jaxx@hotmail.com>
2024-02-02 16:01:37 +0800

    feat: allow search, year, type filters for GET endpoint

--- community_product/views.py
+++ community_product/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import CommunityProduct
 from .serializers import CommunityProductSerializer
 from rest_framework.pagination import PageNumberPagination
@@ -16,6 +16,22 @@ class CommunityProductCreateView(generics.CreateAPIView):
 
 
 class CommunityProductDetailView(generics.ListAPIView):
-    queryset = CommunityProduct.objects.all()  # TODO: order by created_at?
     serializer_class = CommunityProductSerializer
     pagination_class = CommunityProductPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["product_name", "product_description"]
+
+    def get_queryset(self):
+        queryset = CommunityProduct.objects.all()
+
+        # Filter by product_year if provided in the query parameters
+        product_year = self.request.query_params.get("product_year", None)
+        if product_year:
+            queryset = queryset.filter(product_year=product_year)
+
+        # Filter by product_type if provided in the query parameters
+        product_type = self.request.query_params.get("product_type", None)
+        if product_type:
+            queryset = queryset.filter(product_type=product_type)
+
+        return queryset
