b7c4ddcd03d28d2b13ea11d912c2fbf9156c2144
eujiaxin <e_jaxx@hotmail.com>
2024-02-06 15:44:12 +0800

    feat: create list serialiser and assign write_only to language

--- community_product/serializers.py
+++ community_product/serializers.py
@@ -2,7 +2,23 @@ from rest_framework import serializers
 from .models import CommunityProduct
 
 
+class CommunityProductListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CommunityProduct
+        fields = [
+            "id",
+            "product_name",
+            "product_description",
+            "product_type",
+            "product_year",
+            "created_at",
+            "date_approved",
+        ]
+
+
 class CommunityProductSerializer(serializers.ModelSerializer):
+    language = serializers.CharField(write_only=True)
+
     class Meta:
         model = CommunityProduct
         fields = [
