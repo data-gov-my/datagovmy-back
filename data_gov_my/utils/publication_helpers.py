5f39c9b6b3f533c47764f739d48db1a74ed074e3
juwaini <juwaini@gmail.com>
2024-11-13 13:10:48 +0800

    fix null body message

--- data_gov_my/utils/publication_helpers.py
+++ data_gov_my/utils/publication_helpers.py
@@ -451,6 +451,7 @@ def send_email_to_subscribers():
             )
 
 def create_token_message(jwt_token):
+    # TODO: handle language preference
     token_message_en = f"""
 Thank you for subscribing!
 
@@ -467,6 +468,7 @@ Most websites send a simple 6-digit OTP for email verification. However, this ap
 
 Therefore, we use JSON Web Tokens (JWTs) instead. These tokens are long enough self-contained and cryptographically secure, meaning they don’t require extra server resources. This makes OpenDOSM faster and more secure for you!
 """
+    return token_message_en
 
 class OpenDosmMail:
     def __init__(self):
