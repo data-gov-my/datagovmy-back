f7a427ec23dbd558b90d2c59d255db3a677147b1
Daniel Zairin <danielzairink@gmail.com>
2025-09-10 15:00:09 +0800

    fix: misc emails should use datagovmy_ses backend

--- data_request/management/commands/run_email_backend.py
+++ data_request/management/commands/run_email_backend.py
@@ -6,19 +6,25 @@ from django.core.management.base import BaseCommand
 
 from post_office import mail
 
+
 class Command(BaseCommand):
     help = "Test email backend"
 
     def handle(self, *args, **options):
+        recipients_env = os.environ.get("TEST_EMAIL_RECIPIENTS", "")
+        recipients = tuple(
+            email.strip() for email in recipients_env.split(",") if email.strip()
+        )
+        print(recipients)
         mail.send(
-            recipients=('juwaini@gmail.com'),
-            subject='test default mail send',
-            message='test default mail send',
+            recipients=recipients,
+            subject="test default mail send",
+            message="test default mail send",
         )
         mail.send(
-            sender=settings.DEFAULT_FROM_EMAIL_DATA_REQUEST,
-            recipients=('juwaini@gmail.com'),
-            subject='test data request',
-            message='test data request',
-            backend='data_request',
+            sender=settings.DATA_GOV_MY_FROM_EMAIL,
+            recipients=recipients,
+            subject="test data request",
+            message="test data request",
+            backend="datagovmy_ses",
         )
